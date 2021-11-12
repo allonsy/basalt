98e3475c4410bb19ecb6b8120ef81b65d913d9fa
Alec Snyder <linuxbash8@gmail.com>
2021-11-11 18:56:53 -0800

    add read public keys

--- src/keys/keyring.rs
+++ src/keys/keyring.rs
@@ -1,4 +1,5 @@
 use super::private::OnDiskPrivateKey;
+use super::public;
 use crate::config;
 use glob::glob;
 
@@ -30,4 +31,34 @@ pub fn get_device_keys() -> Vec<OnDiskPrivateKey> {
     }
 
     priv_keys
+}
+
+pub fn get_public_keys() -> Vec<public::FullPublicKey> {
+    let mut pub_key_dir = config::get_public_keys_dir();
+    pub_key_dir.push("*.pub");
+
+    let mut pub_keys = Vec::new();
+
+    let glob_matches = glob(pub_key_dir.to_str().unwrap());
+    if let Err(e) = glob_matches {
+        eprintln!("Unable to read public key directory: {}", e);
+        return pub_keys;
+    }
+
+    let glob_matches = glob_matches.unwrap();
+
+    for entry in glob_matches {
+        match entry {
+            Ok(file_path) => {
+                let parsed_priv_key = public::FullPublicKey::read_key(&file_path);
+                match parsed_priv_key {
+                    Ok(key) => pub_keys.push(key),
+                    Err(e) => eprintln!("Unable to read public key: {}", e),
+                };
+            }
+            Err(e) => eprintln!("Unable to read public key: {}", e),
+        }
+    }
+
+    pub_keys
 }
